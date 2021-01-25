be7eb1ef422bb21f0d672bf8add973c757305cd2
Gabriel Pickard <wergomat@gmail.com>
2021-01-24 17:10:24 -0800

    Transfer from allowance, maxPerWallet, calc price via dollar

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -4,13 +4,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Presale is Ownable {
-    IERC20 public MFI;
-    // TODO update presale supply
-    uint public constant presaleSupply = 0;
+    IERC20 public immutable MFI;
+    uint public constant presaleSupply = 5000000;
+    uint public presaleIssued = 0;
+
     // TODO update start date
     uint public constant startDate = 0;
-    // TODO update presalePrice
-    uint256 public constant presalePrice = 1;
+    uint public constant endDate = startDate + 2 days;
+
+    // TODO update dollarsPerEth to most recent value
+    uint public constant dollarsPerETH = 1381;
+    uint public constant tokensPerDollar = 4;
+    uint256 public constant tokensPerETH = dollarsPerETH * tokensPerDollar;
+
+    uint public constant maxPerWallet = (10 ether * tokensPerETH);
 
     constructor(IERC20 tokenContract) public {
         MFI = tokenContract;
@@ -18,11 +25,15 @@ contract Presale is Ownable {
 
     receive() external payable {
         require(startDate <= block.timestamp, "Presale hasn't started yet");
+        require(endDate >= block.timestamp, "Presale is over");
 
-        uint tokensToTransfer = msg.value / presalePrice;
-        require(tokensToTransfer <= MFI.balanceOf(address(this)), "Not enough tokens in presale contract");
+        uint tokensToTransfer = msg.value * tokensPerETH;
+        presaleIssued += tokensToTransfer;
+
+        require(presaleSupply >= presaleIssued, "Not enough tokens in presale contract");
+        require(maxPerWallet >= tokensToTransfer + MFI.balanceOf(msg.sender), "Wallet exceeds max presale");
 
         payable(owner()).transfer(msg.value);
-        MFI.transfer(msg.sender, tokensToTransfer);
+        MFI.transferFrom(owner(), msg.sender, tokensToTransfer);
     }
 }
