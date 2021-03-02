50d75df935052949d6c541b3c81431427a072376
Gabriel Pickard <wergomat@gmail.com>
2021-03-02 17:01:14 -0600

    Remove truffle, add network configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import * as fs from 'fs';
 
 
 
@@ -13,9 +14,44 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
     }
 });
 
+const homedir = require('os').homedir();
+const privateKey = fs.readFileSync(`${homedir}/.marginswap-secret`).toString().trim();
+function infuraUrl(networkName: string) {
+    return `https://${networkName}.infura.io/v3/ae52aea5aa2b41e287d72e10b1175491`;
+}
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-    solidity: "0.8.1",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            blockGasLimit: 12000000
+        },
+        mainnet: {
+            url: infuraUrl('mainnet'),
+            accounts: [privateKey]
+        },
+        kovan: {
+            url: infuraUrl('kovan'),
+            accounts: [privateKey],
+            gas: "auto",
+            gasMultiplier: 1.3,
+            gasPrice: "auto"
+        },
+        ropsten: {
+            url: infuraUrl('ropsten'),
+            accounts: [privateKey]
+        }
+    },
+    solidity: {
+        version: "0.8.1",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200000
+            }
+        }
+    },
 };
