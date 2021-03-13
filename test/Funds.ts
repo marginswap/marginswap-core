d8c24fc42e56e5b8a178493ceafe33ee792434cc
Vincent Raerek <vincent@monadnomad.dev>
2021-03-12 16:51:52 -0800

    Update Funds tests

--- test/Funds.ts
+++ test/Funds.ts
@@ -1,5 +1,5 @@
 const { expect, assert } = require("chai");
-const { ethers, deployments } = require('hardhat');
+const { ethers, deployments, getNamedAccounts } = require('hardhat');
 import ERC20PresetMinterPauser from "@openzeppelin/contracts/build/contracts/ERC20PresetMinterPauser.json";
 
 const ADDRESS_ONE = '0x0000000000000000000000000000000000000001'
@@ -8,8 +8,17 @@ describe("Funds.activateToken", function () {
     beforeEach(async () => {
         await deployments.fixture();
     });
-    it("", async function () {
-        // const Token = await ethers.getContractFactory(ERC20PresetMinterPauser.abi);
-        // const token = await Token.deploy();
+    it("should allow a valid activator to activate a token", async function () {
+        const Token = await ethers.getContractFactory(ERC20PresetMinterPauser.abi, ERC20PresetMinterPauser.bytecode);
+        const token = await Token.deploy("MFITestToken", "MFITT");
+        await token.deployed();
+
+        const Fund = await deployments.get("Fund");
+        const fund = await ethers.getContract("Fund", Fund.address);
+
+        const [owner] = await ethers.getSigners();
+
+        console.log(owner.address);
+        await fund.connect(owner.address).activateToken(token.address);
     });
 });
