b7bd622cb2c1cf1a03e605f0125fa5862b29b787
Gabriel Pickard <wergomat@gmail.com>
2021-03-16 19:46:40 -0500

    Add to dependency controller deployment

--- deploy/DependencyController.ts
+++ deploy/DependencyController.ts
@@ -1,22 +1,52 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from 'hardhat';
 
-const deploy: DeployFunction = async function ({
-  getNamedAccounts,
-  deployments,
-  getChainId,
-  getUnnamedAccounts,
-  network,
-}: HardhatRuntimeEnvironment) {
+type ManagedContract = {
+  contractName: string,
+  charactersPlayed: number[],
+  rolesPlayed: number[],
+  ownAsDelegate?: string[];
+};
+
+const managedContracts: ManagedContract[] = [
+];
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    getNamedAccounts,
+    deployments,
+    getChainId,
+    getUnnamedAccounts,
+    network,
+  } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
   const Roles = await deployments.get("Roles");
 
-  await deploy("DependencyController", {
+  const DependencyController = await deploy("DependencyController", {
     from: deployer,
     args: [Roles.address],
   });
+
+  managedContracts.forEach((mC) => {
+    manage(hre, DependencyController.address, mC);
+  });
+
+  const roles = await ethers.getContractAt("Roles", Roles.address);
+  roles.transferOwnership(DependencyController.address);
 };
 module.exports.tags = ["DependencyController", "local"];
 module.exports.dependencies = ["Roles"];
 export default deploy;
+
+
+async function manage(hre: HardhatRuntimeEnvironment, dcAddress: string, mC: ManagedContract) {
+  const contract = await hre.deployments.get(mC.contractName)
+    .then(C => ethers.getContractAt(mC.contractName, C.address));
+  const dC = await ethers.getContractAt("DependencyController", dcAddress);
+
+  await dC.manageContract(contract.address, mC.charactersPlayed, mC.rolesPlayed, mC.ownAsDelegate || []);
+  await contract.transferOwnership(dC.address);
+
+}
