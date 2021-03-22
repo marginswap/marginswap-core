a498a1cb84d1a3e33b1e166c0e8ed83ecd0a1140
Gabriel Pickard <gpickard@uos.de>
2021-03-22 17:45:41 -0500

    no Console import

--- deploy/DependencyController.ts
+++ deploy/DependencyController.ts
@@ -1,7 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers, network } from 'hardhat';
-import { Console } from "node:console";
 
 type ManagedContract = {
   contractName: string,
