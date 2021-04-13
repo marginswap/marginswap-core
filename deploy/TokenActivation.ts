440104b17a1dc5e671960adae2653f2263c109b1
Gabriel Pickard <wergomat@gmail.com>
2021-04-13 00:44:48 -0500

    token activation for the poors

--- deploy/TokenActivation.ts
+++ deploy/TokenActivation.ts
@@ -10,7 +10,7 @@ const UNI_FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 
 const UNI_INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
 
-const tokensPerNetwork = {
+const tokensPerNetwork: Record<string, Record<string, string>> = {
   kovan: {
     //    USDT: USDT_ADDRESS,
     DAI: '0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa',
@@ -24,7 +24,7 @@ const tokensPerNetwork = {
     UNI: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
     MKR: '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
     USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
-    BOND: "0x0391D2021f89DC339F60Fff84546EA23E337750f",
+    BOND: '0x0391D2021f89DC339F60Fff84546EA23E337750f',
     LINK: '0x514910771af9ca656af840dff83e8264ecf986ca',
     USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
     WBTC: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
@@ -87,7 +87,7 @@ const tokenParams: { [tokenName: string]: TokenInitRecord } = {
     exposureCap: 50000,
     lendingBuffer: 100,
     incentiveWeight: 1,
-    liquidationTokenPath: ['BOND', 'WETH'],
+    liquidationTokenPath: ['BOND', 'USDC'],
     decimals: 18
   },
   LINK: {
@@ -96,7 +96,7 @@ const tokenParams: { [tokenName: string]: TokenInitRecord } = {
     incentiveWeight: 1,
     liquidationTokenPath: ['LINK', 'WETH'],
     decimals: 18,
-    ammPath: [AMMs.SUSHISWAP, AMMs.SUSHISWAP, AMMs.SUSHISWAP]
+    ammPath: [AMMs.SUSHISWAP, AMMs.UNISWAP]
   },
   USDC: {
     exposureCap: 100000000,
@@ -129,7 +129,6 @@ const tokenParams: { [tokenName: string]: TokenInitRecord } = {
     ammPath: [AMMs.SUSHISWAP, AMMs.SUSHISWAP, AMMs.SUSHISWAP]
   },
 
-
   LOCALPEG: {
     exposureCap: 1000000,
     lendingBuffer: 10000,
@@ -159,60 +158,29 @@ const deploy: DeployFunction = async function ({
   const networkName = network.live ? network.name : 'local';
   const peg = (await deployments.get('Peg')).address;
 
-  //  const tokens = network.live ? tokensPerNetwork[networkName] : { LOCALPEG: peg };
-  /*
-  const tokens = network.live
-    ? tokensPerNetwork[networkName]
-    : tokensPerNetwork['mainnet'];
-  */
   const tokens = network.live ? tokensPerNetwork[networkName] : tokensPerNetwork['mainnet'];
-
-  const tokenAddresses = Object.values(tokens);
   const tokenNames = Object.keys(tokens);
+  const tokenAddresses = Object.values(tokens);
 
-  const exposureCaps = tokenNames.map(name => {
-    return ethers.utils.parseUnits(`${tokenParams[name].exposureCap}`, tokenParams[name].decimals);
-  });
-
-  const lendingBuffers = tokenNames.map(name => {
-    return ethers.utils.parseUnits(`${tokenParams[name].lendingBuffer}`, tokenParams[name].decimals);
-  });
-  const incentiveWeights = tokenNames.map(name => tokenParams[name].incentiveWeight);
-
-  const liquidationTokens = tokenNames.map(name => {
-    const tokenPath = tokenParams[name].liquidationTokenPath;
-    return tokenPath ? [...tokenPath.map(tName => tokens[tName]), peg] : [tokens[name], weth, peg];
-  });
-
-  const liquidationAmms = tokenNames.map(name =>
-    tokenParams[name].ammPath ? encodeAMMPath(tokenParams[name].ammPath) : ethers.utils.hexZeroPad('0x00', 32)
-  );
-
-  const Roles = await deployments.get('Roles');
-  const roles = await ethers.getContractAt('Roles', Roles.address);
-
-  const args = [
-    roles.address,
-    tokenAddresses,
-    exposureCaps,
-    lendingBuffers,
-    incentiveWeights,
-    liquidationAmms,
-    liquidationTokens
+  const argLists = [
+    await prepArgs(tokenNames.slice(0, 5), tokenAddresses.slice(0,5), deployments, tokens, peg, weth),
+    //await prepArgs(tokenNames.slice(5, 10), tokenAddresses.slice(5,10), deployments, tokens, peg, weth),
+    //await prepArgs(tokenNames.slice(10), tokenAddresses.slice(10), deployments, tokens, peg, weth)
   ];
+  for (const args of argLists) {
+    const TokenActivation = await deploy('TokenActivation', {
+      from: deployer,
+      args,
+      log: true,
+      skipIfAlreadyDeployed: true
+    });
 
-  const TokenActivation = await deploy('TokenActivation', {
-    from: deployer,
-    args,
-    log: true,
-    skipIfAlreadyDeployed: true
-  });
-
-  // run if it hasn't self-destructed yet
-  if ((await ethers.provider.getCode(TokenActivation.address)) !== '0x') {
-    console.log(`Executing token activation ${TokenActivation.address} via dependency controller ${dc.address}`);
-    const tx = await dc.executeAsOwner(TokenActivation.address);
-    console.log(`ran ${TokenActivation.address} as owner, tx: ${tx.hash}`);
+    // run if it hasn't self-destructed yet
+    if ((await ethers.provider.getCode(TokenActivation.address)) !== '0x') {
+      console.log(`Executing token activation ${TokenActivation.address} via dependency controller ${dc.address}`);
+      const tx = await dc.executeAsOwner(TokenActivation.address);
+      console.log(`ran ${TokenActivation.address} as owner, tx: ${tx.hash}`);
+    }
   }
 
   const TREASURY = '0xB3f923eaBAF178fC1BD8E13902FC5C61D3DdEF5B';
@@ -243,3 +211,37 @@ const deploy: DeployFunction = async function ({
 deploy.tags = ['TokenActivation', 'local'];
 deploy.dependencies = ['DependencyController'];
 export default deploy;
+
+async function prepArgs(tokenNames: string[], tokenAddresses: string[], deployments, tokens, peg, weth) {
+  const exposureCaps = tokenNames.map(name => {
+    return ethers.utils.parseUnits(`${tokenParams[name].exposureCap}`, tokenParams[name].decimals);
+  });
+
+  const lendingBuffers = tokenNames.map(name => {
+    return ethers.utils.parseUnits(`${tokenParams[name].lendingBuffer}`, tokenParams[name].decimals);
+  });
+  const incentiveWeights = tokenNames.map(name => tokenParams[name].incentiveWeight);
+
+  const liquidationTokens = tokenNames.map(name => {
+    const tokenPath = tokenParams[name].liquidationTokenPath;
+    return tokenPath ? [...tokenPath.map(tName => tokens[tName]), peg] : [tokens[name], weth, peg];
+  });
+
+  const liquidationAmms = tokenNames.map(name =>
+    tokenParams[name].ammPath ? encodeAMMPath(tokenParams[name].ammPath) : ethers.utils.hexZeroPad('0x00', 32)
+  );
+
+  const Roles = await deployments.get('Roles');
+  const roles = await ethers.getContractAt('Roles', Roles.address);
+
+  const args = [
+    roles.address,
+    tokenAddresses,
+    exposureCaps,
+    lendingBuffers,
+    incentiveWeights,
+    liquidationAmms,
+    liquidationTokens
+  ];
+  return args;
+}
