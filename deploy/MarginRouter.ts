dca9c7d4fe9480c55e24c6e27c725949e4d7bb81
Gabriel Pickard <wergomat@gmail.com>
2021-05-31 19:27:34 -0500

    marginrouter deploy amm3

--- deploy/MarginRouter.ts
+++ deploy/MarginRouter.ts
@@ -10,14 +10,16 @@ const deploy: DeployFunction = async function ({
   network
 }: HardhatRuntimeEnvironment) {
   const { deploy } = deployments;
-  const { deployer, baseCurrency, amm1Factory, amm2Factory } = await getNamedAccounts();
+  const { deployer, baseCurrency, amm1Factory, amm2Factory, amm3Factory } = await getNamedAccounts();
   const Roles = await deployments.get('Roles');
 
   const amm1InitHash = amm1InitHashes[await getChainId()];
   const amm2InitHash = amm2InitHashes[await getChainId()];
+  const amm3InitHash = amm3InitHashes[await getChainId()];
+
   await deploy('MarginRouter', {
     from: deployer,
-    args: [baseCurrency, amm1Factory, amm2Factory, amm1InitHash, amm2InitHash, Roles.address],
+    args: [baseCurrency, amm1Factory, amm2Factory, amm3Factory, amm1InitHash, amm2InitHash, amm3InitHash, Roles.address],
     log: true,
     skipIfAlreadyDeployed: true
   });
